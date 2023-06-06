319de1f8d97c042d054a8bb2e1846909e9c50be9
santhosh-graceson <119423698+santhosh-graceson@users.noreply.github.com>
2023-06-06 17:44:18 +0530

    Update df.py

--- df.py
+++ df.py
@@ -12,34 +12,34 @@ while True:
         data = [5*math.sin(math.radians(0)),5*math.sin(math.radians(9)),5*math.sin(math.radians(13.5)),5*math.sin(math.radians(18)),5*math.sin(math.radians(22.5)),5*math.sin(math.radians(27)),5*math.sin(math.radians(31.5)),5*math.sin(math.radians(36)),5*math.sin(math.radians(40.5)),5*math.sin(math.radians(45))]
    
         df_Tidal_Volume = df_Tidal_Volume.append({"time": time, "Tidal_Volume": data[0]}, ignore_index=True)
-        t.sleep(1)
+        t.sleep(0.05)
         df_Tidal_Volume = df_Tidal_Volume.append({"time": time, "Tidal_Volume": data[1]}, ignore_index=True)
         
-        t.sleep(1)
+        t.sleep(0.05)
         df_Tidal_Volume = df_Tidal_Volume.append({"time": time, "Tidal_Volume": data[2]}, ignore_index=True)
         
-        t.sleep(1)
+        t.sleep(0.05)
         df_Tidal_Volume = df_Tidal_Volume.append({"time": time, "Tidal_Volume": data[3]}, ignore_index=True)
         
-        t.sleep(1)
+        t.sleep(0.05)
         df_Tidal_Volume = df_Tidal_Volume.append({"time": time, "Tidal_Volume": data[4]}, ignore_index=True)
         
-        t.sleep(1)
+        t.sleep(0.05)
         df_Tidal_Volume = df_Tidal_Volume.append({"time": time, "Tidal_Volume": data[5]}, ignore_index=True)
         
-        t.sleep(1)
+        t.sleep(0.05)
         df_Tidal_Volume = df_Tidal_Volume.append({"time": time, "Tidal_Volume": data[6]}, ignore_index=True)
         
-        t.sleep(1)
+        t.sleep(0.05)
         df_Tidal_Volume = df_Tidal_Volume.append({"time": time, "Tidal_Volume": data[7]}, ignore_index=True)
         
-        t.sleep(1)
+        t.sleep(0.05)
         df_Tidal_Volume = df_Tidal_Volume.append({"time": time, "Tidal_Volume": data[8]}, ignore_index=True)
         
-        t.sleep(1)
+        t.sleep(0.05)
         df_Tidal_Volume = df_Tidal_Volume.append({"time": time, "Tidal_Volume": data[9]}, ignore_index=True)
         
-        t.sleep(1)
+        t.sleep(0.05)
         
         st.write(df_Tidal_Volume)
     except:
